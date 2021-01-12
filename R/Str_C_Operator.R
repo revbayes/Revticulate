3fc3e5e6a675739df95a5ff658007c971aa97b9e
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-01-11 19:24:16 -0600

    Delete R directory

--- R/Str_C_Operator.R
+++
@@ -1,4 +0,0 @@
-#'Operator to streamline pasting. Uses stringr::str_c for speed
-#'@param a The first string to concatenate
-#'@param b The second string to concatenate
-`%+%` <- function(a, b) stringr::str_c(a, b, sep = "")
