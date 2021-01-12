bdec4945483e296396a685da19407f11753f5ff8
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-01-11 19:24:16 -0600

    Delete R directory

--- R/ClearRev.R
+++
@@ -1,6 +0,0 @@
-#'Clears all objects from revenv besides RevPath
-#'@export
-ClearRev <- function(){
-  remove(list = ls(envir = revenv)[which(stringr::str_detect(ls(envir = revenv),
-                                                             "RevPath", negate = TRUE))], envir = revenv)
-}
