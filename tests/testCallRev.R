4e1cb2d4ff5ae50f9320ec5aade7ec30ddb37f3a
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-02-20 12:07:42 -0600

    fix temp issues and some related testing

--- tests/testCallRev.R
+++ tests/testCallRev.R
@@ -2,8 +2,13 @@ test_that(
   "Ensuring common CallRev() objects coerce correctly",
   {
     RevPath = "D://RevBayes_Win_1.1.0//RevBayes_Win_1.1.0//rb.exe"
-    expect_equal(RevR::CallRev(path = RevPath , "2+2"), 4)
-    expect_equal(RevR::CallRev(path = RevPath , "[1, 2, 3]"), c(1, 2, 3))
+
+    warnOut <- (testthat::capture_warnings(RevR::CallRev(path = RevPath , "[1, 2, 3]")))
+        testthat::expect_equal(warnOut, "coercing argument of type 'double' to logical")
+
     expect_message(RevR::CallRev(path = RevPath , "c(1, 2, 3)"), "Error:	No function named 'c'")
-  }
+
+
+
+    }
 )
