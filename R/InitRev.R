63ba33e33e7e97b27d7b08e09e7cc193ecf683d4
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-02-20 12:46:33 -0600

    Text document for storing rb.exe path to avoid having to reenter it

--- R/InitRev.R
+++ R/InitRev.R
@@ -1,5 +1,10 @@
 #'Create an environment for interacting with RevBayes
 #'
+#'Creates an environment for interecting with rb.exe. This environment contains the variable
+#'    RevPath for storing the path to rb.exe. If no path is provided, it will default to trying
+#'    to use a path stored in RevR/RevPath.txt in .libPaths(). This means the user only has
+#'    to provide the path to rb.exe once, and can change the path at any time by applying it again
+#'    in InitRev().
 #'
 #'@param path String path to rb.exe
 #'
@@ -8,11 +13,19 @@
 #'InitRev(RevPath)
 #'
 #'@export
-InitRev <- function(path){
+InitRev <- function(path = NULL){
   RevEnv <<- new.env(parent = globalenv())
-  RevEnv$RevPath <- path
-  RevEnv$Vars <- c()
-  RevEnv$Deterministic <- c()
-  RevEnv$temps <- c()
+
+    if(!is.null(path)){
+      RevEnv$RevPath <- path
+      write(path, list.files(.libPaths(), "RevR", full.names = TRUE) %+% "/RevPath.txt")
+    }
+    else{
+      RevEnv$RevPath <-  readLines(list.files(.libPaths(), "RevR", full.names = TRUE) %+% "/RevPath.txt")
+    }
+
+    RevEnv$Vars <- c()
+    RevEnv$Deterministic <- c()
+    RevEnv$temps <- c()
 }
 
