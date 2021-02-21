bc207b3a717b2dce05b11429d70871bc4fcbcaa9
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-02-20 19:32:25 -0600

    no longer delete temps with ClearRev(); make CallRev() more suitable for tests

--- tests/testCallRev.R
+++ tests/testCallRev.R
@@ -1,14 +1,16 @@
+library(testthat)
 test_that(
-  "Ensuring common CallRev() objects coerce correctly",
+  "Ensure CallRev() coerces as expected",
   {
     RevPath = "D://RevBayes_Win_1.1.0//RevBayes_Win_1.1.0//rb.exe"
+    RevR::InitRev()
 
-    warnOut <- (testthat::capture_warnings(RevR::CallRev(path = RevPath , "[1, 2, 3]")))
-        testthat::expect_equal(warnOut, "coercing argument of type 'double' to logical")
+    #testthat::capture_warnings(RevR::CallRev(path = RevPath , "[1, 2, 3]"))
+        testthat::expect_warning(RevR::CallRev(path = RevPath , "[1, 2, 3]"), "coercing argument of type 'double' to logical")
 
     expect_message(RevR::CallRev(path = RevPath , "c(1, 2, 3)"), "Error:	No function named 'c'")
 
 
-
+    testthat::expect_equal(RevR::doRev("simTree(2)"), ape::read.tree(text = "   (Taxon_1[&index=2]:1.000000,Taxon_2[&index=1]:1.000000)[&index=3]:0.000000;"))
     }
 )
