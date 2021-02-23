4695ab66b24ca98baaf9a6bb57fc1bc0fc2a1de5
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-02-23 12:14:32 -0600

    more testing

--- tests/testdoRev.R
+++ tests/testdoRev.R
@@ -1,6 +1,11 @@
 test_that(
-  "Ensuring common CallRev() objects coerce correctly",
+  "Testing doRev()",
   {
+    testthat::expect_equal(doRev("2+2", "3+3"), list(4, 6))
+
+    expect_message(RevR::RevDefine("myList <- 2+2"), "Numeric object 'myList' has been created in RevEnv!")
+
+    testthat::expect_equal(RevR::doRev("simTree(2)"), ape::read.tree(text = "   (Taxon_1[&index=2]:1.000000,Taxon_2[&index=1]:1.000000)[&index=3]:0.000000;"))
 
   }
 )
