28b17b699a9018e7730b619d403d2b9225f6e2b9
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-03-16 21:19:04 -0500

    Fix numeric coercion error

--- R/KnitRev.R
+++ R/KnitRev.R
@@ -1,13 +1,13 @@
-#' knitr engine for revbayes
+#' Knitr engine for RevBayes
 #'
-#' @param options argument required for knitr engines. options$code coerces the text
-#' in the code chunk to a string, which is then evaluated with doRev()
+#' @param options Argument required for Knitr engines. options$code coerces the text
+#' in the code chunk to a string, which is then evaluated with doRev().
 #'
 #'@export
 KnitRev <- function(){
         knitr::knit_engines$set(rb = function(options) {
         code <- options$code
         if (options$eval)
-          RevR::doRev(code, coerce = F) else code
+          RevR::doRev(code, coerce = FALSE) else code
          })
 }
