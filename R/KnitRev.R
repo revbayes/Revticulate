91892d10521ad4f60feac295a467dec4cba22b8c
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-03-22 13:11:40 -0500

    trying to fix knitting

--- R/KnitRev.R
+++ R/KnitRev.R
@@ -1,15 +1,20 @@
-
 #' Knitr engine for RevBayes
 #'
-#' @param options Argument required for Knitr engines. options$code coerces the text
-#' in the code chunk to a string, which is then evaluated with doRev().
+#'Rev code is ran directly in knitr chunks, and using the wrapper functions isn't necessary.
+#'Any created variables will be put in RevEnv, and defined variables can be used across multiple
+#'chunks.
+#'
 #'
 #'@export
 KnitRev <- function(){
   knitr::knit_engines$set(rb = function(options) {
+
     code <- options$code
-    output <- function() stringr::str_c(RevR::doRev(code, coerce = FALSE, knit = TRUE), sep = " ")
+    output <- stringr::str_c(RevR::doRev(code, coerce = FALSE, knit = TRUE), sep = " ")
+
     if (options$eval)
-      stringr::str_c(output(), collapse = " \n") else code
+      return(stringr::str_c(output, collapse = " \n"))
+    else code
+
   })
 }
