6704d2cc61cbdcf5c2b69bc2a44536cd6fc0d15e
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-04-19 17:09:21 -0500

    fix knit

--- R/KnitRev.R
+++ R/KnitRev.R
@@ -12,13 +12,14 @@ KnitRev <- function(){
 
     lastOutput <- RevR::CallRev(RevEnv$allCode, coerce = FALSE, knit = TRUE)
 
-      RevEnv$allCode <- c(RevEnv$allCode, options$code)
+    RevEnv$allCode <- c(RevEnv$allCode, options$code)
 
     nextOutput <- RevR::CallRev(RevEnv$allCode, coerce = FALSE, knit = TRUE)
 
-      output <- nextOutput[-c(1:length(lastOutput))]
+    output <- nextOutput[-c(1:length(lastOutput))]
+
+    code <- options$code
 
     return(knitr::engine_output(options, code = options$code, out = output))
-
   })
 }
