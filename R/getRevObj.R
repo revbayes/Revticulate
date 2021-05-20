6010e9cb8768f8aa5f704dc08b86f3003a407b3a
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-05-20 17:24:04 -0500

    drastically simplifying Rev object definitions

---
+++ R/getRevObj.R
@@ -0,0 +1,15 @@
+#'Getter function to retrieve objects created in rb.exe.
+#'
+#'@param name String. Name of object to retrieve
+#'
+#'@param coerce boolean. If true, Revticulate attempts to coerce the String formatted Rev object into
+#'                       a comparable R object. Default is FALSE.
+#'
+#'
+#'@export
+#'
+
+
+getRevObj <- function(name, coerce = FALSE){
+  return(cleanCallRev(name, viewCode = FALSE, coerce = coerce))
+}
