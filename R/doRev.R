e5c051dc295530b820d7be479a27c07edb285581
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-05-22 00:06:12 -0500

    test vectorized rev history

--- R/doRev.R
+++ R/doRev.R
@@ -12,7 +12,7 @@ doRev <- function(input, viewCode = FALSE, coerce = FALSE){
 
 
   revEnv$allCode <- unlist(c(revEnv$allCode, input))
-  revEnv$allCode <- stringr::str_c(revEnv$allCode, collapse = "\n")
+  #revEnv$allCode <- stringr::str_c(revEnv$allCode, collapse = "\n")
 
 
   first <- callRev(getRevHistory(), coerce = F)
