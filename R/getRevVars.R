31703df1a0bae4b34b684cbb40e11660ca2e2f2d
W0650629 <57684423+W0650629@users.noreply.github.com>
2021-05-22 00:27:37 -0500

    make revEnv$vars camelcase

--- R/getRevVars.R
+++ R/getRevVars.R
@@ -1,12 +1,12 @@
 #'Get variable definitions from RevEnv
 #'
-#'Prints revEnv$Vars to return definitions of variables that have been defined in rb.exe
+#'Prints revEnv$vars to return definitions of variables that have been defined in rb.exe
 #'    using a Revticulate function.
 #'
 #'@export
 #'
 getRevVars <- function(){
 
-  return(revEnv$Vars)
+  return(revEnv$vars)
 
 }
