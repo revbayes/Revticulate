0db287c807b48a5749254cc40e5ed21a5d0aa644
April Wright <wright.aprilm@gmail.com>
2021-06-03 09:05:04 -0500

    add functionality to remove specified number of lines. Update tutorial to show off.

--- R/ClearRev.R
+++ R/ClearRev.R
@@ -1,11 +1,15 @@
 
 #'Empties revEnv
 #'
-#'Clears allCode and vars from revEnv.
+#'Clears all code, or a user-specified amount of code, and vars from revEnv.
 #'
+#'@param n How many lines to remove. Should be an integer
 #'
+#'@examples
+#' clearRev() #Clear all objects from RevBayes
+#' clearRev(n = 1) # Clear the last line input to RevBayes
 #'@export
-clearRev <- function(){
+clearRev <- function(n = NULL){
 
   #prevent temp file list from getting too large
   if(length(revEnv$temps) > 50){
@@ -14,9 +18,17 @@ clearRev <- function(){
 
   cat("", file = revEnv$revHistory, append = F)
 
+  if (is.null(n)){
   remove(list = ls(envir = revEnv)[which(ls(envir = revEnv) != "RevPath" & ls(envir = revEnv) != "temps" & ls(envir = revEnv) != "revHistory")],
          envir = revEnv)
 
   message("Successfully reset revEnv!")
+  } else if (!is.null(n)){
+    file = readLines(revEnv$revHistory)
+    remove = length(file)-n
+    edited = file[-(remove : length(file))]
+    writeLines(revEnv$revHistory)
+    callRev(revEnv$revHistory)
+  }
 
 }
