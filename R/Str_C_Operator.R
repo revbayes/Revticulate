666d676442a32f328f4966b4265a39f17c1bfe4c
April Wright <wright.aprilm@gmail.com>
2021-06-07 21:24:53 -0500

    fixed vignette issue + operator documentation

--- R/Str_C_Operator.R
+++ R/Str_C_Operator.R
@@ -3,10 +3,13 @@
 #'Method for quicker pasting of Strings.
 #'
 #'@rdname stringpaste
-#'@usage \%+\%(a, b)
 #'@title Operator for concatenation.
 #'@param a The first String
 #'@param b The second String
 #'
+#'@examples
+#' \dontrun{
+#' \%+\%(a, b) append string b to string a
+#' }
 #'@export
 "%+%" <- function(a, b) stringr::str_c(a, b, sep = "")
