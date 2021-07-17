6d785ba3b2a2515d0953a69b7c801879243df2b1
Caleb Charpentier <caleb.charpentier@selu.edu>
2021-07-17 15:13:44 -0500

    fix clearRev error with numbers; add tests for callRev

--- tests/testthat/testclearRev.R
+++ tests/testthat/testclearRev.R
@@ -1,8 +1,19 @@
 library(testthat)
+library(Revticulate)
+library(comprehenr)
+
 test_that(
   "Testing ClearRev()",
   {
     Revticulate::initRev()
+
+    for(i in 1:10){
+      pow <- 2**as.integer(runif(1, 1, 10))
+      doRev("simTree(" %+% pow %+% ")")
+    }
+
+    clearRev(1)
+
 
 
   }
