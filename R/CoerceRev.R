ffc427a6921f80745d2c87894bc4637fa9e88bd2
Caleb Charpentier <caleb.charpentier@selu.edu>
2022-02-08 16:39:09 -0600

    lots of coercion updates, fix repRep() prompt format, add dependencies

--- R/CoerceRev.R
+++ R/CoerceRev.R
@@ -20,6 +20,7 @@
 #'@import comprehenr
 #'@import phytools
 #'@import jsonlite
+#'@import usethis
 #'
 #' @export
 #'
@@ -29,42 +30,86 @@ coerceRev <- function(revString){
 
   rObj <- stringr::str_squish(revString)
 
-  if(length(revString) > 1)
-    rObj <- rObj[which(rObj != "")]
+  if(length(revString) > 1){
+    rObj <- rObj[rObj != ""]
+  }
 
-  if(jsonlite::validate(rObj) == TRUE){
-    rObj <- fromJSON(rObj)
-    return(rObj)
+  if(length(rObj) == 0){
+    if(stringr::str_detect(rObj, "^inf?")){
+      return(Inf)
+    }
+    if(stringr::str_detect(rObj, "^NULL?")){
+      return(NULL)
+    }
+    if(stringr::str_detect(rObj, "^nan?")){
+      return(NA)
+    }
+    if(stringr::str_detect(rObj, "^TRUE?")){
+      return(TRUE)
+    }
+    if(stringr::str_detect(rObj, "^FALSE?")){
+      return(FALSE)
+    }
+    if(paste0(rObj) == ""){
+      return(invisible(""))
+    }
+  }
+
+  count <- 0
+  copy <- c()
+  for(i in rObj){
+
+      if(count < 0){
+        lastElement <- copy[length(copy)]
+        copy[length(copy)] <- paste0(lastElement, i)
+      }
+      else{
+        copy <- append(copy, i)
+      }
+      count <- count - stringr::str_count(i, "\\(")
+      count <- count + stringr::str_count(i, "\\)")
+
+    }
+  rObj <- copy
+
+  validateTree <- function(treeString) {
+    simplifiedTree <- stringr::str_remove_all(treeString, "[^)(,]")
+
+    openCount <- sum(stringr::str_count(simplifiedTree, "\\("))
+    closeCount <- sum(stringr::str_count(simplifiedTree, "\\)"))
+
+    if(openCount == closeCount & openCount > 1){
+      return(TRUE)
+    }
+    else{
+      return(FALSE)
+    }
   }
 
   toJSONVector <- function(stringArg) {
     return(stringr::str_c("[", stringArg, "]"))
   }
 
-  if(jsonlite::validate(toJSONVector(rObj)) == TRUE){
-    rObj <- fromJSON(toJSONVector(rObj))
+  detectNumeric <- function(x){
+    stringr::str_detect(x, "^[-]?[0-9]+[.]?[0-9]*|[-]?[0-9]+[L]?|[-]?[0-9]+[.]?[0-9]*[eE][0-9]+$")
+  }
+
+  if(jsonlite::validate(rObj) == TRUE){
+    rObj <- fromJSON(rObj)
     return(rObj)
   }
 
-  validateTree <- function(treeString) {
-    simplifiedTree <- stringr::str_remove_all(tree1, "[^)(,]")
-
-    openCount <- stringr::str_count(simplifiedTree, "\\(")
-    closeCount <- stringr::str_count(simplifiedTree, "\\)")
-
-    if(openCount == closeCount & openCount > 1){
-      return(TRUE)
-    }
-    return(FALSE)
-  }
-
-  if(validateTree(rObj)){
+  else if(validateTree(rObj)){
     rObj <- ape::read.tree(text = rObj)
     return(rObj)
   }
 
-  if(length(rObj) == 0)
-     return(invisible())
+  if(all(detectNumeric(rObj))){
+    return(as.numeric(rObj))
+  }
+
   return(rObj)
+
 }
+
 
