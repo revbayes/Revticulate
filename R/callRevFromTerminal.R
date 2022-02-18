6c7dfb094495275f8c4a5b49555570b70e9a8a78
Caleb Charpentier <caleb.charpentier@selu.edu>
2022-02-18 15:47:12 -0600

    callRev uses current wd

--- R/callRevFromTerminal.R
+++ R/callRevFromTerminal.R
@@ -9,9 +9,19 @@
 callRevFromTerminal <- function(revscript){
   rbPath <- Sys.getenv("rb")
   script <- normalizePath(revscript, winslash = "/")
+  wd <- normalizePath(getwd(), winslash = "/")
   if(Sys.info()['sysname'] == 'Windows'){
     script <- str_replace_all(script, "/", "//")
+    wd <- str_replace_all(wd, "/", "//")
   }
+
+  his <- getRevHistory()
+  his[1] <- paste0('source("', wd, '")')
+  cat(his, file=Sys.getenv("revHistory"), sep = "\n", append = FALSE)
+
   termID = rstudioapi::terminalExecute(paste(rbPath, script))
+
+  his[1] <- paste0('#START\n')
+  cat(his, file=Sys.getenv("revHistory"), sep = "\n", append = FALSE)
   return(termID)
 }
